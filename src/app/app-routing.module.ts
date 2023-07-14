43d153c7fd7cac544e11d87b79b474c2400f7a73
stanleyBAH <ho_stanley@bah.com>
2023-07-14 14:17:33 -0400

    created routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
-// NgModule: typescript decorator used to create an angular module.
 import { NgModule } from '@angular/core';
 // Routes and RouterModule: RouterModule provides static methods like RouterModule.forRoot() for passing a configuration object to the Router.
 import { Routes, RouterModule } from '@angular/router';
 
-// The constant 'routes' array of type Routes which will be used to hold information for each route.
-const routes: Routes = [];
+//// Components for the router.
+import { PeopleListComponent } from './people-list/people-list.component';
+import { PeopleDetailComponent } from './people-detail/people-detail.component';
 
-// AppRoutingModule class uses the @NgModule decorator that takes some metadata.
+// The constant 'routes' array of type Routes which will be used to hold information for each route.
+const routes: Routes = [
+  { path: 'people', component: PeopleListComponent },
+  { path: 'people/:id', component: PeopleDetailComponent },
+];
+
 @NgModule({
   // Here, we call the static RouterModule.forRoot(routes) method with the routes array as a parameter.
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
+// AppRoutingModule class uses the @NgModule decorator that takes some metadata.
 export class AppRoutingModule {}
